5437e7cdd5576b40e4ced9c124c92b4bcb84894d
theOnlyColm <theOnlyColm@gmail.com>
2020-06-27 22:06:07 -0700

    fix

--- iot4beginners.com.py
+++ iot4beginners.com.py
@@ -17,5 +17,5 @@ def analogInput(channel):
 while True:
   output = analogInput(1) # Reading from CH0
   x = datetime.datetime.now()
-  print("ADC reading: {} {} ({}V)".format(output,x))
+  print("ADC reading: {} {}".format(output,x))
   sleep(1)
